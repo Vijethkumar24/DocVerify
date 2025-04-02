724f712177a0022fd4315a92d5fd1a310c6bdd05
vin1112 <vkvijethkumar24@gmail.com>
2025-04-02 17:38:02 +0530

    removed nodemodules

--- server.js
+++ server.js
@@ -365,5 +365,5 @@ app.get("/uploadDoc", (req, res) => {
 });
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${port}`);
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
